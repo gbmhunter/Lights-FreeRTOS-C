db54da028ce1f4278c586fe096e078c277f76fcc
Geoffrey Hunter <gbmhunter@gmail.com>
2012-10-02 18:46:06 +1300

    Moved code into ./src/. Modified comments.

--- src/Lights.c
+++ src/Lights.c
@@ -8,13 +8,15 @@
 //!		<b>Last Modified:			</b> 27/11/2011					\n
 //!		<b>Version:					</b> v1.0.0						\n
 //!		<b>Company:					</b> CladLabs					\n
-//!		<b>Project:					</b> Example Code				\n
-//!		<b>Computer Architecture:	</b> ARM						\n
+//!		<b>Project:					</b> Free Code Modules			\n
+//!		<b>Language:				</b> C							\n
 //!		<b>Compiler:				</b> GCC						\n
 //! 	<b>uC Model:				</b> PSoC5						\n
+//!		<b>Computer Architecture:	</b> ARM						\n
 //! 	<b>Operating System:		</b> FreeRTOS v7.2.0			\n
 //!		<b>Documentation Format:	</b> Doxygen					\n
-//!		
+//!		<b>License:					</b> GPLv3						\n
+//!			
 
 //===============================================================================================//
 //========================================= INCLUDES ============================================//
